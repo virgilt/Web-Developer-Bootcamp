0622393f9887e3ff9840580f0623f7fe3ad98e93
Travis Virgil <travis@travisvirgil.com>
2018-06-07 15:22:28 -0400

    DOM Manipulation Exercises

---
+++ DOM Manipulation/selectorExercise/selectorExercise.js
@@ -0,0 +1,4 @@
+var first = document.getElementById("first");
+var special = document.getElementsByClassName("special")[0];
+var queryFirst = document.querySelector("#first");
+var querySpecial = document.querySelector(".special");
